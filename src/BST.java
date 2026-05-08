4691561f6020c88bd8c409c24d917b1d566f718d
Abdallah Mohamed Abdelsattar <abdoelnil605@gmail.com>
2026-05-08 17:39:40 +0300

    finished remove, added print

--- src/BST.java
+++ src/BST.java
@@ -22,6 +22,8 @@ public class BST {
     public void insert(Person person){
         if (isEmpty()){
             root = new BSTNode(person, null, null, null);
+            size++;
+            return;
         }
         BSTNode current = root;
         BSTNode parent = null;
@@ -46,6 +48,80 @@ public class BST {
             parent.setLeft(newNode);
         } else {
             parent.setRight(newNode);
+        }
+
+        size++;
+    }
+
+    public BSTNode search(String targetName) {
+        return recursiveSearch(root, targetName);
+    }
+
+    private BSTNode recursiveSearch(BSTNode current, String targetName) {
+        if (current == null) {
+            return null;
+        }
+
+        int cmp = targetName.toLowerCase().compareTo(current.getPerson().getPersonName().toLowerCase());
+
+        if (cmp == 0) {
+            return current;
+        } else if (cmp < 0) {
+            return recursiveSearch(current.getLeft(), targetName);
+        } else {
+            return recursiveSearch(current.getRight(), targetName);
+        }
+
+    }
+
+    public void remove(String targetName) {
+
+        BSTNode nodeToKill = search(targetName);
+
+        if (nodeToKill == null) {
+            System.out.println("Contact not found");
+            return;
+        }
+
+        kill(nodeToKill);
+        size--;
+    }
+
+    private void kill(BSTNode node) {
+
+        if (node.getLeft() == null || node.getRight() == null) {
+            BSTNode child = (node.getLeft() != null) ? node.getLeft() : node.getRight();
+
+            if (node.getParent() == null) {
+                root = child;
+            } else if (node == node.getParent().getLeft()) {
+                node.getParent().setLeft(child);
+            } else {
+                node.getParent().setRight(child);
+            }
+
+            if (child != null) {
+                child.setParent(node.getParent());
+            }
+        }
+        else {
+            BSTNode successor = getMin(node.getRight());
+            node.setPerson(successor.getPerson());
+            kill(successor);
+        }
+    }
+
+    private BSTNode getMin(BSTNode node) {
+        while (node.getLeft() != null)
+            node = node.getLeft();
+        return node;
+    }
+
+    public void printAlphabetical(BSTNode node) {
+        if (node != null) {
+            printAlphabetical(node.getLeft());
+            System.out.println(node.getPerson());
+            printAlphabetical(node.getRight());
         }
     }
 }
